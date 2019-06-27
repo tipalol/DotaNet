d2cc3e1f906590a98a7f04d262fcbc38922212cc
sorokindmitrij <tipalol2@gmail.com>
2019-06-27 17:02:59 +0500

    Added LoadPage function to Parser
    This function can load HTML content from Web

--- DotaNet/Classes/Parser/Parser.cs
+++ DotaNet/Classes/Parser/Parser.cs
@@ -1,8 +1,45 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using HtmlAgilityPack;
+
 namespace DotaNet.Classes.Parser
 {
+    /// <summary>
+    /// Класс <see cref="Parser"/> реализует выгрузку данных из сайта
+    /// </summary>
     public class Parser
     {
+        /// <summary>
+        /// Фукнция выгружает содержимое страницы
+        /// </summary>
+        /// <param name="url">Адресс загружаемой страницы</param>
+        /// <returns>Содержимое страницы</returns>
+        public static string LoadPage(string url)
+        {
+            var result = "";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var recieveStream = response.GetResponseStream();
+                if (recieveStream != null)
+                {
+                    StreamReader streamReader;
+                    if (response.CharacterSet == null)
+                        streamReader = new StreamReader(recieveStream);
+                    else
+                        streamReader = new StreamReader(recieveStream, Encoding.GetEncoding(response.CharacterSet));
+                    result = streamReader.ReadToEnd();
+                    streamReader.Close();
+                }
+                response.Close();
+            }
+
+            return result;
+        }
         public Parser()
         {
         }
