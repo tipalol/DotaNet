8e3287d4135ed53a51de35d13c075ff903ed481d
sorokindmitrij <tipalol2@gmail.com>
2019-06-27 15:38:42 +0500

    Created

---
+++ DotaNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotaNet
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
